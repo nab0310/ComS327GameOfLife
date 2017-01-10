2897087a5dfb59aecd2c5955b77737c6c2bfe35a
Nick Behrens <behrensn@iastate.edu>
2017-01-10 09:54:57 -0600

    Moving init array to another function

--- helloWorld.c
+++ helloWorld.c
@@ -2,17 +2,22 @@
 int WIDTH = 24;
 int LENGTH = 80;
 
+void init_array(int array[WIDTH][LENGTH]){
+    int i,j;
+    for(i=0;i<WIDTH;i++){
+        for(j=0;j<LENGTH;j++){
+            array[i][j] = 1;
+        }
+    }
+}
+
 int main(){
   int array[WIDTH][LENGTH];
-  int i,j;
 
   printf("Hello World!\n");
 
-  for(i=0;i<WIDTH;i++){
-      for(j=0;j<LENGTH;j++){
-          array[i][j] = 1;
-      }
-  }
+  init_array(array);
+
   int k,l;
 
   for(k=0;k<WIDTH;k++){
