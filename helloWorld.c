25633fb1e48597f3183e2334747e3b891ee046da
nab0310 <nabehrens@sbcglobal.net>
2017-01-09 19:06:23 -0600

    Changed segemetation fault?

--- helloWorld.c
+++ helloWorld.c
@@ -5,8 +5,8 @@ int LENGTH = 80;
 void init_array(int array[WIDTH][LENGTH]){
   int i=0;
   int j=0;
-  for(i=0;i<LENGTH;i++){
-    for(j=0;j<WIDTH;j++){
+  for(i=0;i<LENGTH-1;i++){
+    for(j=0;j<WIDTH-1;j++){
       array[i][j] = 1;
     }
   }
@@ -15,8 +15,8 @@ void init_array(int array[WIDTH][LENGTH]){
 void print_array(int array[WIDTH][LENGTH]){
   int i=0;
   int j=0;
-  for (i = 0; i < LENGTH; i++) {
-    for(j = 0; j< WIDTH; j++){
+  for (i = 0; i < LENGTH-1; i++) {
+    for(j = 0; j< WIDTH-1; j++){
       if(array[i][j]==1){
         printf("-");
       }
