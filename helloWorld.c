75f26e2265ecfd1655acc7a8c1b64673506f0f43
nab0310 <nabehrens@sbcglobal.net>
2017-01-09 18:49:54 -0600

    Defined ints Length and Width

--- helloWorld.c
+++ helloWorld.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
+int WIDTH = 24;
+int LENGTH = 80;
 
 main(){
- printf("Hello World!\n");
+
+  printf("Hello World!\n");
+  printf("Width: "+WIDTH + "\n");
+  printf("Length: "+LENGTH +"\n");
 }
